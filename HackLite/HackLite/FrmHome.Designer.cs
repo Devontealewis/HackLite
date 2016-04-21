73426a41bee39ed02010e0011168d48e1c6ea57c
devontealewis <dl01757@georgiasouthern.edu>
2016-04-20 23:55:18 -0400

    Tab two now called Dos

--- HackLite/HackLite/FrmHome.Designer.cs
+++ HackLite/HackLite/FrmHome.Designer.cs
@@ -136,7 +136,7 @@ namespace HackLite
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(525, 333);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "ARP Cache Poison";
+            this.tabPage2.Text = "DOS";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
